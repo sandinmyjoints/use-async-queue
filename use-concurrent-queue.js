5e5ec23b61f50740b3ab79d5ac05d5677c5174e8
William Bert <william.bert@gmail.com>
2020-04-18 15:47:26 -0400

    wip

--- use-concurrent-queue.js
+++ use-concurrent-queue.js
@@ -1,6 +1,35 @@
-import { useState, useCallback } from 'react';
-export default function useConcurrentQueue() {
-  const [count, setCount] = useState(0);
-  const increment = useCallback(() => setCount((x) => x + 1), []);
-  return { count, increment };
+import { useCallback, useEffect } from 'react';
+import { useQueue } from 'react-use';
+
+/*
+task
+{id: string or number, task: function returning promise}
+*/
+export default function useConcurrentQueue({ concurrency, done }) {
+  if (concurrency < 1) concurrency = 1;
+
+  // { add, remove, first, last, size }
+  const inFlight = useQueue();
+  const remaining = useQueue();
+
+  const add = useCallback(
+    (task) => {
+      remaining.add(task);
+    },
+    [remaining]
+  );
+
+  useEffect(() => {
+    while (inFlight.size < concurrency && remaining.size > 0) {
+      const task = remaining.remove();
+      inFlight.add(task);
+      const promise = task.task();
+      promise.then(() => {
+        inFlight.remove(task);
+        done({ ...task, promise });
+      });
+    }
+  }, [concurrency, done, inFlight, remaining]);
+
+  return { add };
 }
--- use-concurrent-queue.test.js
+++ use-concurrent-queue.test.js
@@ -1,8 +1,25 @@
-import { renderHook } from '@testing-library/react-hooks';
-import useCounter from './use-concurrent-queue';
+import { renderHook, act } from '@testing-library/react-hooks';
+import useConcurrentQueue from './use-concurrent-queue';
 
-it('should use counter', () => {
-  const { result } = renderHook(() => useCounter());
-  expect(result.current.count).toBe(0);
-  expect(typeof result.current.increment).toBe('function');
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+it('should initialize it', () => {
+  const { result } = renderHook(() => useConcurrentQueue({ concurrency: 1 }));
+  expect(typeof result.current.add).toBe('function');
+});
+
+it('should use it', () => {
+  const done = jest.fn();
+  const task = {
+    id: 1,
+    task: () => Promise.resolve('1 is done'),
+  };
+  const { result } = renderHook(() =>
+    useConcurrentQueue({ concurrency: 1, done })
+  );
+  expect(done).not.toHaveBeenCalled();
+  act(() => result.current.add(task));
+  expect(done).toHaveBeenCalledTimes(1);
 });
