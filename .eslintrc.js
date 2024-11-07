d8ef3e70e53c290a90142410fce7e3039b6428cc
William Bert <william.bert@gmail.com>
2024-11-07 12:01:46 -0500

    Prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,31 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    'jest/globals': true,
+    "jest/globals": true
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: ['react', 'jest', 'react-hooks'],
+  plugins: ["react", "jest", "react-hooks"],
   rules: {
-    'prettier/prettier': ['error', { singleQuote: true }],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
   overrides: [
     {
-      files: ['*.ts'],
-      plugins: ['@typescript-eslint'],
-      extends: ['plugin:@typescript-eslint/recommended'],
-      parser: '@typescript-eslint/parser',
-    },
-  ],
+      files: ["*.ts"],
+      plugins: ["@typescript-eslint"],
+      extends: ["plugin:@typescript-eslint/recommended"],
+      parser: "@typescript-eslint/parser"
+    }
+  ]
 };
